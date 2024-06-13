12276c45df8915e7277184f6a181ed3aa22d4381
akhitha-mb <akhiakhitha3@gmail.com>
2024-06-13 15:47:12 +0530

    created

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ const express=require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const bcryptjs=require("bcryptjs")
+const jsonwebtoken=require("jsonwebtoken")
 const {signupmodel}=require("./models/Signup")
 
 const app = express()
@@ -27,8 +28,51 @@ app.post("/signup",async(req,res)=>{
     let blog=new signupmodel(input)
     blog.save()
        
-    res.send("success")
+    res.json({"status":"success"})
 })
+app.post("/signin",(req,res)=>{
+    let input=req.body
+    signupmodel.find({"email":req.body.email}).then(
+        (response)=>{
+           if (response.length>0) {
+           
+
+            let dbPassword=response[0].password
+            console.log(dbPassword)
+            bcryptjs.compare(input.password,dbPassword,(error,ismatch)=>{
+                if (ismatch) {
+                    jsonwebtoken.sign({email:input.emailid},"signupmodel",{expiresIn:"1d"},(error,token)=>{
+                        if (error) {
+                            res.json({status:"anable to create token"})
+                            
+                        } else {
+
+                            res.json({status:"success","userId":response[0]._id,"token":token})
+                        }
+                    })
+                   
+                } else {
+                    res.json({status:"incorrect password"})
+                    
+                }
+            })
+
+           } else {
+            res.json({status:"user not found"})
+            
+           }
+        }
+    ).catch()
+
+
+
+
+
+})
+
+
+
+
 app.listen(8083, () => {
     console.log("server started")
 })
